66d5daeb2b1e0cc8534a41be1ef8309c8eb19384
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-09 21:07:56 +0530

    This is super basic so just wasting time here

---
+++ Conditionals.java
@@ -0,0 +1,12 @@
+public class Conditionals {
+
+  public static void main(String[] args) {
+    if (true) {
+      System.out.println("Sorry");
+    } else if (1 < 2) {
+      System.out.println("IDK why I am doing this");
+    } else {
+      System.out.println("Too many warnings");
+    }
+  }
+}
