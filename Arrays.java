ac656870b02d2ac3ed9dc23d94c8d1b86210cccd
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-11 12:40:54 +0530

    Add loops

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+
+  public static void main(String[] args) {
+    int[] marks0 = new int[5];
+    marks0[0] = 100;
+    marks0[1] = 90;
+    marks0[2] = 80;
+    marks0[3] = 65;
+    marks0[4] = 70;
+    int[] marks1 = { 90, 80, 98, 70, 75 };
+    System.out.println(marks0[0]);
+    System.out.println(marks1[3]);
+  }
+}
--- Loops.java
+++ Loops.java
@@ -1,11 +1,11 @@
 public class Loops {
 
   public static void main(String[] args) {
-    //int i = 0;
-    //while (i < 10) {
-    //System.out.println(i);
-    //i++;
-    //}
+    // int i = 0;
+    // while (i < 10) {
+    // System.out.println(i);
+    // i++;
+    // }
 
     for (int i = 0; i < 10; i++) {
       if (i == 2) {
@@ -16,11 +16,10 @@ public class Loops {
         break;
       } else System.out.println(i);
     }
-    //int i = 0;
-    //do {
-    //System.out.println(i);
-    //i++;
-    //} while (i < 10);
-
+    // int i = 0;
+    // do {
+    // System.out.println(i);
+    // i++;
+    // } while (i < 10);
   }
 }
