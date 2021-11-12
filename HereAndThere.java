9e013b88b91e4f0f38c5868e9b1d0be3c21db91e
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-12 20:45:58 +0530

    More new things here with static and IntParse()

---
+++ HereAndThere.java
@@ -0,0 +1,10 @@
+public class HereAndThere {
+
+  public static void main(String[] args) {
+    // Parsing string to int
+    // String numStr = "123";
+    // int numInt = 7;
+    // int numInt2 = Integer.parseInt(numStr);
+    // System.out.println(numInt2 + numInt);
+  }
+}
--- Methods.java
+++ Methods.java
@@ -4,16 +4,23 @@ public class Methods {
     return x + y;
   }
 
-  //static float sum(int a, int b) {
-  //System.out.println("This is an overloaded function");
-  //return 10.04F;
-  //}
+  // static float sum(int a, int b) {
+  // System.out.println("This is an overloaded function");
+  // return 10.04F;
+  // }
 
-  static float sum(int x) {
+  float sum(int x) {
     return x;
   }
 
   public static void main(String[] args) {
     System.out.println(sum(10, 20));
+    // System.out.println(sum(10)); // cannot make a static refrence to a non static method
+    Methods obj0 = new Methods();
+    // the float sum method will be created as many times we create a new instance of the Methods class
+    // but the static int sum class will only be created once
+    // Methods obj1 = new Methods();
+    // Methods obj2 = new Methods();
+    System.out.println(obj0.sum(10));
   }
 }
