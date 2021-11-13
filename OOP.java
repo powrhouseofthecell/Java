6d79ef509f29706daf0b015091fe7cc7a9667524
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-13 19:04:32 +0530

    OOP java

--- OOP.java
+++ OOP.java
@@ -1,16 +1,21 @@
 class myClass {
-   int id;
-   String name;
-   public String details() {
-      return "The name under the id " + id + " is " + name;
-   }
+
+  int id;
+  String name;
+
+  public String details() {
+    return "The name under the id " + id + " is " + name;
+  }
 }
+
 public class OOP {
-   public static void main(String[] args) {
-      myClass obj = new myClass();
-      obj.name = "Zuhaib";
-      obj.id = 12;
-      System.out.println(obj.id);
-      System.out.println(obj.name);
-   }
+
+  public static void main(String[] args) {
+    myClass obj = new myClass();
+    obj.name = "Zuhaib";
+    obj.id = 12;
+    System.out.println(obj.id);
+    System.out.println(obj.name);
+  }
 }
+
