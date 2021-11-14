e986415f27b9562e577596583c3d26864e770758
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-14 14:33:15 +0530

    Formatted

--- OOP.java
+++ OOP.java
@@ -18,4 +18,3 @@ public class OOP {
     System.out.println(obj.name);
   }
 }
-
--- Strings.java
+++ Strings.java
@@ -11,10 +11,10 @@ public class Strings {
     // printf works the same as it did in C
     // There is also System.out.format() which works in the same way
     System.out.printf(
-    "The value of a is %d and the value of b is %f and the value of c is %c",
-    a,
-    b,
-    c
+      "The value of a is %d and the value of b is %f and the value of c is %c",
+      a,
+      b,
+      c
     );
 
     // System.out.println(name);
