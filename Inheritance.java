46c22b8987ea3bf9a6c531ae5ac13237e8ec5c15
Zuhaib <theshahzuhaib96@gmail.com>
2021-11-15 21:11:22 +0530

    Constructors in inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -1,35 +1,55 @@
 class Base {
-    public int x;
 
-    public int getX() {
-        return x;
-    }
+  public int x;
 
-    public void setX(int x) {
-        this.x = x;
-    }
+  Base() {
+    System.out.println("Base class no args constructor");
+  }
+
+  Base(int a) {
+    System.out.println("Base constructor with args a = " + a);
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int xVal) {
+    x = xVal;
+  }
 }
 
-class Derived extends  Base {
-    public int y;
+class Derived extends Base {
 
-    public int getY() {
-        return y;
-    }
+  public int y;
 
-    public void setY(int y) {
-        this.y = y;
-    }
+  Derived() {
+    super(10);
+    System.out.println("Derived class not args constructor");
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int yVal) {
+    y = yVal;
+  }
 }
+
 public class Inheritance {
-    public static void main(String[] args) {
-        Base b = new Base();
-        Derived d = new Derived();
-        b.x = 10;
-        d.y = 20;
-        System.out.println(b.getX());
-        System.out.println(d.getX()); // set in b not d
-        d.x = 100;
-        System.out.println(d.getX());
-    }
+
+  public static void main(String[] args) {
+    //Base b = new Base();
+    Derived d = new Derived();
+    //
+    //b.x = 10;
+    //d.y = 20;
+    //System.out.println(b.getX());
+    //System.out.println(d.getX()); // set in b not d
+    //d.x = 100;
+    //System.out.println(d.getX());
+    //
+
+  }
 }
